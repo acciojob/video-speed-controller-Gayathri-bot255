6f99df9443f25d967b159cee21c91fe42048640c
GANDLA MALDAKAL GAYATHRI <gmgayathrigayathri@gmail.com>
2026-09-16 22:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
-const inputs = document.querySelectorAll('.controls input');
+```javascript
+const video = document.querySelector("video");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+const playerButton = document.querySelector(".player__button");
+
+const volume = document.querySelector(".volume");
+
+const playbackSpeed = document.querySelector(".playbackSpeed");
+
+const skipButtons = document.querySelectorAll(".skip");
+
+const progressFilled = document.querySelector(".progress__filled");
+
+
+// Play and Pause
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playerButton.textContent = "❚ ❚";
+    } else {
+        video.pause();
+        playerButton.textContent = "►";
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+playerButton.addEventListener("click", togglePlay);
+
+
+// Volume
+volume.addEventListener("input", function () {
+    video.volume = this.value;
+});
+
+
+// Playback Speed
+playbackSpeed.addEventListener("input", function () {
+    video.playbackRate = this.value;
+});
+
+
+// Skip 10 seconds / 25 seconds
+skipButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+
+        const skipTime = this.dataset.skip;
+
+        video.currentTime += Number(skipTime);
+
+    });
+});
+
+
+// Progress Bar
+video.addEventListener("timeupdate", function () {
+
+    const percentage =
+        (video.currentTime / video.duration) * 100;
+
+    progressFilled.style.width = percentage + "%";
+
+});
+
+
+// Click on progress bar
+const progress = document.querySelector(".progress");
+
+progress.addEventListener("click", function (event) {
+
+    const position =
+        event.offsetX / progress.offsetWidth;
+
+    video.currentTime = position * video.duration;
+
+});
+
+
+// Update button when video ends
+video.addEventListener("ended", function () {
+    playerButton.textContent = "►";
+});
+```
